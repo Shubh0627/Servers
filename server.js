8e6a07abcd5b93152695d78e6125fb0351cfc5b4
Shubh Shukla <shubhshukla9347@gmail.com>
2025-07-08 15:44:02 +0530

    bug fix

--- server.js
+++ server.js
@@ -45,6 +45,10 @@ app.use('/api/status', (req, res)=> res.send("Server is live"));
 app.use("/api/auth", userRouter)
 app.use("/api/messages", messageRouter)
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the Chat App Server");
+});
+
 //Connect to MongoDB
 await connectDB()
 
